52c613c50b40ac55d403b730b8e968e0d6b83852
spaceInvaderBuzz <kingofpiratesjosh@gmail.com>
2024-06-24 18:29:17 -0700

    message 12345

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,3 @@ export default defineConfig({
     }
   }
 })
-
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
-})
